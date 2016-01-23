70951587b4aa13825fcf103db61a885e3aae01cb
世平阜康 <512769206@qq.com>
2016-01-24 02:41:14 +0800

    新版更新

--- README.md
+++ README.md
@@ -1,85 +1,183 @@
+![Yelee](
+http://i13.tietuku.com/404b8c63eb155793.jpg)
 
-　　** Hexo 主题：**用了yilia主题一段时间，感觉还有很多可以提高的地方，就查阅资料，对其进行粗类的修改，但是，有其实还有很多不完善的地方，欢迎大家前捧场。
-没想到，这么多人喜欢黑色版本的，建议不是每个人都喜欢我的这些功能，所以准备个基础版本，插件可以看教程自行安装。
+### Introduction 前言
 
-[文章链接](http://luuman.github.io/2015/12/21/GitHub+Hexo/)   
+Theme **Yelee** relies on [Hexo-Theme-Yilia][1], thanks for the author [Litten][2]. Fix some bugs, change lots of styles, add several features. And then I made the theme. Yelee is mainly designed for fluent text reading. I change styles and add functions, meanwhile, try hard to keep this theme simple, stupid and clear. Theme DEMO: [MOxFIVE's Blog][6]
 
-hexo-theme-spfk
-================
-          
-主题结构：
+> M-Hexo-Blog [Commits][3]; hexo-theme-yelee [Commits][4]; [建站日志][5]
 
-    .
-    ├── languages         #多语言
-    |   ├── default.yml  #默认语言
-    |   └── zh-CN.yml    #中文语言
-    ├── layout           #布局，根目录下的*.ejs文件是对主页，分页，存档等的控制
-    |   ├── _partial     #局部的布局，此目录下的*.ejs是对头尾等局部的控制
-    |   └── _widget      #小挂件的布局，页面下方小挂件的控制
-    ├── source           #源码
-    |   ├── css          #css源码 
-    |   |   ├── _base    #*.styl基础css
-    |   |   ├── _partial    #*.styl局部css
-    |   |   ├── fonts       #字体
-    |   |   ├── images      #图片
-    |   |   └── style.styl #*.styl引入需要的css源码
-    |   ├── fancybox      #fancybox效果源码
-    |   └── js            #javascript源代码
-    ├── _config.yml       #主题配置文件
-    └── README.md         #用GitHub的都知道
+本主题 **Yelee** 基于主题 [Hexo-Theme-Yilia][1] 修改而来，在此感谢原作者 [Litten][2]。修复了一些 bugs，改变了大量的样式，添加了不少特性。对原主题百般折腾后，发觉变动越来越大，索性就发布个新主题了，主题随我微博名叫 "Yelee" 。个人喜欢简洁的样式，重视内容的浏览，同时希望博客作为个人网站，能稍微凸显出博主个性。各种修改折腾大抵基于以上考虑。主题实际效果，请访问本人博客查看：[MOxFIVE's Blog][6]
 
+[1]: https://github.com/litten/hexo-theme-yilia
+[2]: http://litten.github.io/ "Litten的博客"
+[3]: https://github.com/MOxFIVE/M-Hexo-Blog/commits/master
+[4]: https://github.com/MOxFIVE/hexo-theme-yelee/commits/master
+[5]: http://moxfive.xyz/2015/08/20/blog-building/ "个人博客站点建设历程"
+[6]: http://moxfive.xyz
 
-![luuman-ipad-iphone](https://raw.githubusercontent.com/luuman/luuman.github.io/master/resoures/luuman-ipad-iphone.jpg)
-![iPhone6](https://raw.githubusercontent.com/luuman/luuman.github.io/master/resoures/iPhone6-mockup.jpg)
+![QR](http://i11.tietuku.com/f9ce6d6bb62921d4.png)
 
-##一、关于主题：
-         
-
-##二、使用
-
-#### 安装
+### Installation 安装主题
 
 ```
-$ git clone https://github.com/luuman/hexo-theme-spfk.git themes/spfk
+git clone https://github.com/MOxFIVE/hexo-theme-yelee.git themes/yelee
 ```
 
-#### 配置
-
-修改hexo根目录下的 `_config.yml` ： `theme: spfk`
-
-#### 更新
+Change theme field in Hexo root's _config.yml file. 修改 Hexo 根目录对应配置文件。
 
 ```
-cd themes/spfk
+theme: yelee
+```
+
+### Update 更新
+
+```
+cd themes/yelee
 git pull
 ```
 
-##三、配置
+### New Features 新特性
+| - |            Chs           |                En               |
+|:-:|:------------------------:|:-------------------------------:|
+| 1 | 嵌入边栏的文章目录       | Flexible table of contents      |
+| 2 | 透明化背景，随机背景大图 | Transparent & Random background |
+| 3 | 页内跳转按钮             | Scrolling button                |
+| 4 | 文章版权等信息显示       | Copyright info.                 |
+| 5 | 文章导航切换按钮         | Post navigation button          |
+| 6 | 网站计数                 | Site counter                    |
 
-主题配置文件在主目录下的`_config.yml`：[_config.yml](http://luuman.github.io/2015/12/21/GitHub+Hexo/)   
+![feature](http://i13.tietuku.com/2bfd34c63f627bae.jpg)
 
+
+### Configuration 配置
+
+#### 0. Post Excerpt 文章摘要
+There are two ways to show excerpt in homepage. 
+
+目前主题可使用两种方式在首页显示文章摘要而不是全文。
+
+- a: <!-- more -->
+
+``` diff
+title: Hello World
+date: 2015-12-03 00:00:00
+---
+<Excerpt in index | 首页摘要> 
++ <!-- more -->
+<The rest of contents | 余下全文>
+```
+- b: description in Front-matter
+
+``` diff
+title: Hello World
+date: 2015-12-03 00:00:00
++ description: "Welcome to Hexo! This is your very first post."
+---
+<Contents>
 ```
 
+> Description only support plain text. | 通过 description 添加的摘要只能为纯文本。
 
-#### Header
-J:\Hexo\Hexo\themes\spfk\_config.yml
-menu:
-  主页: /
-  所有文章: /archives
-  # 随笔: /tags/随笔
+> Set the value of description with quotes to avoid unexpected error `:`. | description 的内容加双引号，可以避免一些程序错误，例如内容里包含英文冒号时。
 
-    #是否开启多说评论，填写你在多说申请的项目名称 duoshuo: duoshuo-key（http://duoshuo-key.duoshuo.com/）
-    #若使用disqus，请在博客config文件中填写disqus_shortname，并关闭多说评论
-    duoshuo: true
 
-J:\Hexo\Hexo\themes\spfk\source\css\多说.css
+
+#### 1. About Page 关于我页面: 
+cd to your hexo folder and run this code:
+
+使用以下代码添加一个新页面：
+
 ```
-#### 头像
- 头像尺寸大概200*200px
+hexo new page about
+```
 
+#### 2. Tags Cloud Page 标签云页面:
 
+```
+hexo new page tags
+```
 
+> Post with several categories 文章设置多个分类后的问题 [issue#4](https://github.com/MOxFIVE/hexo-theme-yelee/issues/4) 
 
-#### Bug
+> - [让 Hexo 自动生成 Tag Cloud 标签云页面](http://moxfive.xyz/2015/10/25/hexo-tag-cloud/)
 
-如有Bug欢迎大家前来反馈！
+#### 3. Background image 网页背景:
+
+Find or change background images in folder | 修改背景图地址: 
+
+> `/yelee/source/background/`
+
+Setting in `themes/yelee/_config.yml` 背景参数:
+
+`
+background_image: 5
+`
+
+- Default value is 5, free to modify the number | 默认值为5，可按需修改
+
+- "5": show 5 images form bg-1.jpg to bg-5.jpg in `/yelee/source/background/`
+
+- "5": 设置`/yelee/source/background/`文件夹中 bg-1.jpg 到 bg-5.jpg 这5张图片为背景
+
+- "0": remove background image and use white-gray theme | 取消网页背景图，使用淳朴的灰白主题 
+
+#### 4. Style image 自定义图片样式:
+In order to style posts/pages' image with HTML/CSS within `.md` files, you should disable fancybox function firstly. 
+
+如果要使用 HTML/CSS 自定义文章图片样式，需要先关闭 fancybox 功能。
+
+Disable fancybox in full site | 在全站关闭 fancybox:
+
+> Set `fancybox: false` in `yelee/_config.yml`
+
+Disable fancybox in certain post/page | 在某篇文章中关闭 fancybox:
+
+> Add `fancybox: false` in [front-matter](https://hexo.io/docs/front-matter.html)
+
+#### 5. Comment 评论:
+Disqus, duoshuo and youyan is supported, enable them in theme's "_config.yml".
+主题目测支持 Disqus，多说 和友言评论，自行在主题配置中开启。
+
+多说: http://duoshuo.com/create-site/ 登陆你的多说并创建站点，在 "domain" 中填入你设定的域名。请设置好自己专属的多说，不要再群聊了。
+
+> - [保留使用 Yilia 主题时的多说用户评论](https://github.com/MOxFIVE/hexo-theme-yelee/issues/1)
+
+> - [多说样式折腾记录 — 添加 UA 浏览器标识、旋转头像等](http://moxfive.xyz/2015/09/29/duoshuo-style/)
+
+#### 6. Table of Contents 文章目录:
+
+Remove toc and the button via putting `toc: false` before "---" at [post].md.
+
+文章中默认显示目录和对应切换按钮，在文章 “---” 前输入 `toc: false` 关闭目录。
+
+#### 7. Copyright info. 文章版权信息:
+
+Hide this  via putting `original: false` to post's front-matter.
+
+在文章顶部插入行 `original: false` 关闭文章版权声明框
+
+#### 8. 404 Page:
+
+```
+hexo new page 404
+```
+And then set `permalink: /404` in `/source/404/index.md` front matter.
+
+> - [在 Hexo 中创建匹配主题的404页面](http://moxfive.xyz/2015/10/16/hexo-404-page/)
+
+#### 9. RSS Feed 文章订阅:
+
+Install plugin: [hexo-generator-feed](https://github.com/hexojs/hexo-generator-feed)
+
+#### 10. Sitemap for SEO 站点地图:
+
+Install plugin: [hexo-generator-seo-friendly-sitemap](https://github.com/ludoviclefevre/hexo-generator-seo-friendly-sitemap)
+
+百度专用: [hexo-generator-baidu-sitemap](https://github.com/coneycode/hexo-generator-baidu-sitemap)
+
+#### 11. Apple Touch icon 苹果图标:
+
+替换路径: `/yelee/source/apple-touch-icon.png`
+
+[Recommended size: 180*180](https://realfavicongenerator.net/blog/apple-touch-icon-the-good-the-bad-the-ugly/)
--- source/js/main.js
+++ source/js/main.js
@@ -1,110 +1,144 @@
 require([], function (){
 
-	var isMobileInit = false;
-	var loadMobile = function(){
-		require(['/js/mobile.js'], function(mobile){
-			mobile.init();
-			isMobileInit = true;
-		});
-	}
-	var isPCInit = false;
-	var loadPC = function(){
-		require(['/js/pc.js'], function(pc){
-			pc.init();
-			isPCInit = true;
-		});
-	}
+    var isMobileInit = false;
+    var loadMobile = function(){
+        require(['/js/mobile.js'], function(mobile){
+            mobile.init();
+            isMobileInit = true;
+        });
+    }
+    var isPCInit = false;
+    var loadPC = function(){
+        require(['/js/pc.js'], function(pc){
+            pc.init();
+            isPCInit = true;
+        });
+    }
 
-	var browser={
-	    versions:function(){
-	    var u = window.navigator.userAgent;
-	    return {
-	        trident: u.indexOf('Trident') > -1, //IE内核
-	        presto: u.indexOf('Presto') > -1, //opera内核
-	        webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
-	        gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
-	        mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
-	        ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
-	        android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
-	        iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者安卓QQ浏览器
-	        iPad: u.indexOf('iPad') > -1, //是否为iPad
-	        webApp: u.indexOf('Safari') == -1 ,//是否为web应用程序，没有头部与底部
-	        weixin: u.indexOf('MicroMessenger') == -1 //是否为微信浏览器
-	        };
-	    }()
-	}
+    var browser={
+        versions:function(){
+        var u = window.navigator.userAgent;
+        return {
+            trident: u.indexOf('Trident') > -1, //IE内核
+            presto: u.indexOf('Presto') > -1, //opera内核
+            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+            android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+            iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者安卓QQ浏览器
+            iPad: u.indexOf('iPad') > -1, //是否为iPad
+            webApp: u.indexOf('Safari') == -1 ,//是否为web应用程序，没有头部与底部
+            weixin: u.indexOf('MicroMessenger') == -1 //是否为微信浏览器
+            };
+        }()
+    }
 
-	$(window).bind("resize", function(){
-		if(isMobileInit && isPCInit){
-			$(window).unbind("resize");
-			return;
-		}
-		var w = $(window).width();
-		if(w >= 700){
-			loadPC();
-		}else{
-			loadMobile();
-		}
-	});
+    $(window).bind("resize", function(){
+        if(isMobileInit && isPCInit){
+            $(window).unbind("resize");
+            return;
+        }
+        var w = $(window).width();
+        if(w >= 700){
+            loadPC();
+        }else{
+            loadMobile();
+        }
+    });
 
-	if(browser.versions.mobile === true || $(window).width() < 700){
-		loadMobile();
-	}else{
-		loadPC();
-	}
+    if(browser.versions.mobile === true || $(window).width() < 700){
+        loadMobile();
+    }else{
+        loadPC();
+    }
 
-	//是否使用fancybox
-	if(yiliaConfig.fancybox === true){
-		require(['/fancybox/jquery.fancybox.js'], function(pc){
-			var isFancy = $(".isFancy");
-			if(isFancy.length != 0){
-				var imgArr = $(".article-inner img");
-				for(var i=0,len=imgArr.length;i<len;i++){
-					var src = imgArr.eq(i).attr("src");
-					var title = imgArr.eq(i).attr("alt");
-					imgArr.eq(i).replaceWith("<a href='"+src+"' title='"+title+"' rel='fancy-group' class='fancy-ctn fancybox'><img src='"+src+"' title='"+title+"'></a>");
-				}
-				$(".article-inner .fancy-ctn").fancybox();
-			}
-		});
-		
-	}
-	//是否开启动画
-	if(yiliaConfig.animate === true){
+    //是否使用fancybox
+    if(yiliaConfig.fancybox === true){
+        require(['/fancybox/jquery.fancybox.js'], function(pc){
+            var isFancy = $(".isFancy");
+            if(isFancy.length != 0){
+                var imgArr = $(".article-inner img");
+                for(var i=0,len=imgArr.length;i<len;i++){
+                    var src = imgArr.eq(i).attr("src");
+                    var title = imgArr.eq(i).attr("alt");
+                    imgArr.eq(i).replaceWith("<a href='"+src+"' title='"+title+"' rel='fancy-group' class='fancy-ctn fancybox'><img src='"+src+"' title='"+title+"'></a>");
+                }
+                $(".article-inner .fancy-ctn").fancybox();
+            }
+        });
 
-		require(['/js/jquery.lazyload.js'], function(){
-			//avatar
-			$(".js-avatar").attr("src", $(".js-avatar").attr("lazy-src"));
-			$(".js-avatar")[0].onload = function(){
-				$(".js-avatar").addClass("show");
-			}
-		});
-		
-		if(yiliaConfig.isHome === true){
-			//content
-			function showArticle(){
-				$(".article").each(function(){
-					if( $(this).offset().top <= $(window).scrollTop()+$(window).height() && !($(this).hasClass('show')) ) {
-						$(this).removeClass("hidden").addClass("show");
-						$(this).addClass("is-hiddened");
-					}else{
-						if(!$(this).hasClass("is-hiddened")){
-							$(this).addClass("hidden");
-						}
-					}
-				});
-			}
-			$(window).on('scroll', function(){
-				showArticle();
-			});
-			showArticle();
-		}
-		
-	}
-	
-	//是否新窗口打开链接
-	if(yiliaConfig.open_in_new == true){
-		$(".article a[href]").attr("target", "_blank")
-	}
-	
+    }
+    //是否开启动画
+    if(yiliaConfig.animate === true){
+
+        require(['/js/jquery.lazyload.js'], function(){
+            //avatar
+            $(".js-avatar").attr("src", $(".js-avatar").attr("lazy-src"));
+            $(".js-avatar")[0].onload = function(){
+                $(".js-avatar").addClass("show");
+            }
+        });
+
+      if(yiliaConfig.isHome === true) {
+        // 滚动条监听使用scrollreveal.js
+        // https://github.com/jlmakes/scrollreveal.js
+        // 使用cdn[//cdn.bootcss.com/scrollReveal.js/3.0.5/scrollreveal.js]
+        require([
+          '//cdn.bootcss.com/scrollReveal.js/3.0.5/scrollreveal.js'
+        ], function (ScrollReveal) {
+          // 更多animation:
+          // http://daneden.github.io/animate.css/
+          var animationNames = [
+            "pulse", "fadeIn","fadeInRight", "flipInX", "lightSpeedIn","rotateInUpLeft", "slideInUp","zoomIn",
+            ],
+            len = animationNames.length,
+            randomAnimationName = animationNames[Math.ceil(Math.random() * len) - 1];
+
+          // ie9 不支持css3 keyframe动画, safari不支持requestAnimationFrame, 不使用随机动画，切回原来的动画
+          if (!window.requestAnimationFrame) {
+              $('.body-wrap > article').css({opacity: 1});
+
+              if (navigator.userAgent.match(/Safari/i)) {
+                  function showArticle(){
+                      $(".article").each(function(){
+                          if( $(this).offset().top <= $(window).scrollTop()+$(window).height() && !($(this).hasClass('show')) ) {
+                              $(this).removeClass("hidden").addClass("show");
+                              $(this).addClass("is-hiddened");
+                          }else{
+                              if(!$(this).hasClass("is-hiddened")){
+                                  $(this).addClass("hidden");
+                              }
+                          }
+                      });
+                  }
+                  $(window).on('scroll', function(){
+                      showArticle();
+                  });
+                  showArticle();
+              }
+              return;
+          }
+          // document.body有些浏览器不支持监听scroll，所以使用默认的document.documentElement
+          ScrollReveal({
+            duration: 0,
+            afterReveal: function (domEl) {
+              // safari不支持requestAnimationFrame不支持document.documentElement的onscroll所以这里不会执行
+              // 初始状态设为opacity: 0, 动画效果更平滑一些(由于脚本加载是异步，页面元素渲染后在执行动画，感觉像是延时)
+              $(domEl).addClass('animated ' + randomAnimationName).css({opacity: 1});
+            }
+          }).reveal('.body-wrap > article');
+
+        });
+      } else {
+        $('.body-wrap > article').css({opacity: 1});
+      }
+
+    }
+
+    //是否新窗口打开链接
+    if(yiliaConfig.open_in_new == true){
+        $(".article a[href]").attr("target", "_blank")
+    }
+    $(".archive-article-title").attr("target", "_blank");
 });
